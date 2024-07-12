bf33bdec202e08cb5ab6388609da80ea58e5f5db
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-07-12 01:28:35 +0000

    Update 73.1.1

---
+++ media/src/main/java/com/truvideo/sdk/media/TruvideoSdkMedia.kt
@@ -0,0 +1,8 @@
+@file:JvmName("TruvideoSdkMedia")
+
+package com.truvideo.sdk.media
+
+import com.truvideo.sdk.media.interfaces.TruvideoSdkMedia
+
+@get:JvmName("getInstance")
+lateinit var TruvideoSdkMedia: TruvideoSdkMedia
