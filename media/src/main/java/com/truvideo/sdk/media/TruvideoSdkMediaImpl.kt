3105a563cb073e8b0edae9eb17320a6a11f78a59
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-08-01 18:36:56 +0000

    Update 73.1.2-dev.50

---
+++ media/src/main/java/com/truvideo/sdk/media/TruvideoSdkMediaImpl.kt
@@ -0,0 +1,332 @@
+package com.truvideo.sdk.media
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.map
+import com.truvideo.sdk.media.builder.TruvideoSdkMediaFileUploadRequestBuilder
+import com.truvideo.sdk.media.engines.TruvideoSdkMediaFileUploadEngine
+import com.truvideo.sdk.media.interfaces.TruvideoSdkMedia
+import com.truvideo.sdk.media.interfaces.TruvideoSdkMediaAuthAdapter
+import com.truvideo.sdk.media.interfaces.TruvideoSdkMediaCallback
+import com.truvideo.sdk.media.interfaces.TruvideoSdkMediaLogAdapter
+import com.truvideo.sdk.media.model.TruvideoSdkMediaFileType
+import com.truvideo.sdk.media.model.TruvideoSdkMediaFileUploadRequest
+import com.truvideo.sdk.media.model.TruvideoSdkMediaFileUploadStatus
+import com.truvideo.sdk.media.model.TruvideoSdkMediaPaginatedResponse
+import com.truvideo.sdk.media.model.TruvideoSdkMediaResponse
+import com.truvideo.sdk.media.model.TruvideoSdkMediaTags
+import com.truvideo.sdk.media.repository.TruvideoSdkMediaFileUploadRequestRepository
+import com.truvideo.sdk.media.service.media.TruvideoSdkMediaService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import truvideo.sdk.common.TruvideoSdkContextProvider
+import truvideo.sdk.common.exception.TruvideoSdkException
+import truvideo.sdk.common.model.TruvideoSdkLogSeverity
+
+internal class TruvideoSdkMediaImpl(
+    context: Context,
+    private val authAdapter: TruvideoSdkMediaAuthAdapter,
+    private val mediaFileUploadRequestRepository: TruvideoSdkMediaFileUploadRequestRepository,
+    private val mediaService: TruvideoSdkMediaService,
+    private val fileUploadEngine: TruvideoSdkMediaFileUploadEngine,
+    private val logAdapter: TruvideoSdkMediaLogAdapter
+) : TruvideoSdkMedia {
+
+    private var scope = CoroutineScope(Dispatchers.IO)
+
+    init {
+        TruvideoSdkContextProvider.instance.init(context)
+
+        logAdapter.addLog(
+            eventName = "event_media_init",
+            message = "Init media module",
+            severity = TruvideoSdkLogSeverity.INFO
+        )
+
+        scope.launch { mediaFileUploadRequestRepository.cancelAllProcessing() }
+    }
+
+    override fun FileUploadRequestBuilder(filePath: String): TruvideoSdkMediaFileUploadRequestBuilder {
+        logAdapter.addLog(
+            eventName = "event_media_file_upload_request_build_create",
+            message = "Building file upload request for path: $filePath",
+            severity = TruvideoSdkLogSeverity.INFO
+        )
+
+        authAdapter.validateAuthentication()
+
+        return TruvideoSdkMediaFileUploadRequestBuilder(
+            filePath = filePath,
+            mediaRepository = mediaFileUploadRequestRepository,
+            engine = fileUploadEngine
+        )
+    }
+
+
+    override fun streamAllFileUploadRequests(
+        status: TruvideoSdkMediaFileUploadStatus?
+    ): LiveData<List<TruvideoSdkMediaFileUploadRequest>> {
+        logAdapter.addLog(
+            eventName = "event_media_file_upload_request_stream_all",
+            message = "Streaming all file upload requests with status: $status",
+            severity = TruvideoSdkLogSeverity.INFO
+        )
+
+        authAdapter.validateAuthentication()
+
+        val stream = mediaFileUploadRequestRepository.streamAll(status)
+        return stream.map { list ->
+            list.onEach { item ->
+                item.engine = fileUploadEngine
+            }
+        }
+    }
+
+    override fun streamAllFileUploadRequests(
+        status: TruvideoSdkMediaFileUploadStatus?,
+        callback: TruvideoSdkMediaCallback<LiveData<List<TruvideoSdkMediaFileUploadRequest>>>
+    ) {
+        scope.launch {
+            try {
+                val data = streamAllFileUploadRequests(status)
+                callback.onComplete(data)
+            } catch (exception: Exception) {
+                val resultException = if (exception is TruvideoSdkException) {
+                    exception
+                } else {
+                    TruvideoSdkException(exception.localizedMessage ?: "Unknown error")
+                }
+                callback.onError(resultException)
+            }
+        }
+    }
+
+
+    override suspend fun getFileUploadRequestById(id: String): TruvideoSdkMediaFileUploadRequest? {
+        logAdapter.addLog(
+            eventName = "event_media_file_upload_request_get_by_id",
+            message = "Getting file upload request by ID: $id",
+            severity = TruvideoSdkLogSeverity.INFO
+        )
+
+        authAdapter.validateAuthentication()
+
+        val request = mediaFileUploadRequestRepository.getById(id) ?: return null
+        request.engine = fileUploadEngine
+        return request
+    }
+
+    override fun getFileUploadRequestById(
+        id: String,
+        callback: TruvideoSdkMediaCallback<TruvideoSdkMediaFileUploadRequest?>
+    ) {
+        scope.launch {
+            try {
+                val request = getFileUploadRequestById(id)
+                callback.onComplete(request)
+            } catch (exception: Exception) {
+                val resultException = if (exception is TruvideoSdkException) {
+                    exception
+                } else {
+                    TruvideoSdkException(exception.localizedMessage ?: "Unknown error")
+                }
+                callback.onError(resultException)
+            }
+        }
+    }
+
+    override fun streamFileUploadRequestById(id: String): LiveData<TruvideoSdkMediaFileUploadRequest?> {
+        logAdapter.addLog(
+            eventName = "event_media_file_upload_request_stream_by_id",
+            message = "Streaming file upload request by ID: $id",
+            severity = TruvideoSdkLogSeverity.INFO
+        )
+
+        authAdapter.validateAuthentication()
+
+        val stream = mediaFileUploadRequestRepository.streamById(id)
+        return stream.map {
+            it?.apply {
+                engine = fileUploadEngine
+            }
+        }
+    }
+
+    override fun streamFileUploadRequestById(
+        id: String,
+        callback: TruvideoSdkMediaCallback<LiveData<TruvideoSdkMediaFileUploadRequest?>>
+    ) {
+        scope.launch {
+            try {
+                val data = streamFileUploadRequestById(id)
+                callback.onComplete(data)
+            } catch (exception: Exception) {
+                val resultException = if (exception is TruvideoSdkException) {
+                    exception
+                } else {
+                    TruvideoSdkException(exception.localizedMessage ?: "Unknown error")
+                }
+                callback.onError(resultException)
+            }
+        }
+    }
+
+    override suspend fun getAllFileUploadRequests(status: TruvideoSdkMediaFileUploadStatus?): List<TruvideoSdkMediaFileUploadRequest> {
+        logAdapter.addLog(
+            eventName = "event_media_file_upload_request_get_all",
+            message = "Getting all file upload requests with status: $status",
+            severity = TruvideoSdkLogSeverity.INFO
+        )
+
+        authAdapter.validateAuthentication()
+
+        val items = mediaFileUploadRequestRepository.getAll(status)
+        items.forEach { it.engine = fileUploadEngine }
+        return items
+    }
+
+
+    override fun getAllFileUploadRequests(
+        status: TruvideoSdkMediaFileUploadStatus?,
+        callback: TruvideoSdkMediaCallback<List<TruvideoSdkMediaFileUploadRequest>>
+    ) {
+        scope.launch {
+            try {
+                val data = getAllFileUploadRequests(status)
+                callback.onComplete(data)
+            } catch (exception: Exception) {
+                val resultException = if (exception is TruvideoSdkException) {
+                    exception
+                } else {
+                    TruvideoSdkException(exception.localizedMessage ?: "Unknown error")
+                }
+                callback.onError(resultException)
+            }
+        }
+    }
+
+    override suspend fun searchById(id: String): TruvideoSdkMediaResponse? {
+        logAdapter.addLog(
+            eventName = "event_media_search_by_id",
+            message = "Search by id: $id",
+            severity = TruvideoSdkLogSeverity.INFO
+        )
+
+        authAdapter.validateAuthentication()
+
+        try {
+            val result = mediaService.fetchAll(
+                tags = TruvideoSdkMediaTags(),
+                idList = listOf(),
+                type = TruvideoSdkMediaFileType.All,
+                pageNumber = 0,
+                pageSize = 1,
+            )
+            return result.data.firstOrNull()
+        } catch (exception: Exception) {
+            logAdapter.addLog(
+                eventName = "event_media_search_by_id",
+                message = "Search by id error: ${exception.localizedMessage}",
+                severity = TruvideoSdkLogSeverity.ERROR
+            )
+
+            exception.printStackTrace()
+
+            if (exception is TruvideoSdkException) {
+                throw exception
+            } else {
+                throw TruvideoSdkException(exception.localizedMessage ?: "Unknown error")
+            }
+        }
+    }
+
+    override fun searchById(
+        id: String,
+        callback: TruvideoSdkMediaCallback<TruvideoSdkMediaResponse?>
+    ) {
+        scope.launch {
+            try {
+                val result = searchById(id)
+                callback.onComplete(result)
+            } catch (exception: Exception) {
+                val resultException = if (exception is TruvideoSdkException) {
+                    exception
+                } else {
+                    TruvideoSdkException(exception.localizedMessage ?: "Unknown error")
+                }
+                callback.onError(resultException)
+            }
+        }
+    }
+
+    override suspend fun search(
+        tags: TruvideoSdkMediaTags,
+        type: TruvideoSdkMediaFileType,
+        pageNumber: Int,
+        pageSize: Int,
+    ): TruvideoSdkMediaPaginatedResponse<TruvideoSdkMediaResponse> {
+        logAdapter.addLog(
+            eventName = "event_media_search",
+            message = "Tags: ${tags.map.entries.joinToString(",") { "${it.key}=${it.value}" }}. Type: ${type.name}. Page: $pageNumber. Size: $pageSize",
+            severity = TruvideoSdkLogSeverity.INFO
+        )
+
+        authAdapter.validateAuthentication()
+
+        try {
+            return mediaService.fetchAll(
+                tags = tags,
+                idList = listOf(),
+                type = type,
+                pageNumber = pageNumber,
+                pageSize = pageSize,
+            )
+        } catch (exception: Exception) {
+            logAdapter.addLog(
+                eventName = "event_media_search",
+                message = "Error searching media: ${exception.localizedMessage}",
+                severity = TruvideoSdkLogSeverity.ERROR
+            )
+
+            exception.printStackTrace()
+
+            if (exception is TruvideoSdkException) {
+                throw exception
+            } else {
+                throw TruvideoSdkException(exception.localizedMessage ?: "Unknown error")
+            }
+        }
+    }
+
+    override fun search(
+        tags: TruvideoSdkMediaTags,
+        type: TruvideoSdkMediaFileType,
+        pageNumber: Int,
+        pageSize: Int,
+        callback: TruvideoSdkMediaCallback<TruvideoSdkMediaPaginatedResponse<TruvideoSdkMediaResponse>>
+    ) {
+        scope.launch {
+            try {
+                val result = search(
+                    tags = tags,
+                    type = type,
+                    pageNumber = pageNumber,
+                    pageSize = pageSize
+                )
+                callback.onComplete(result)
+            } catch (exception: Exception) {
+                val resultException = if (exception is TruvideoSdkException) {
+                    exception
+                } else {
+                    TruvideoSdkException(exception.localizedMessage ?: "Unknown error")
+                }
+                callback.onError(resultException)
+            }
+        }
+    }
+
+
+    override val environment: String
+        get() = BuildConfig.FLAVOR
+}
---
+++ media/src/main/java/com/truvideo/sdk/media/interfaces/TruvideoSdkMediaFileUploadCallback.kt
@@ -0,0 +1,36 @@
+package com.truvideo.sdk.media.interfaces
+
+import com.truvideo.sdk.media.model.TruvideoSdkMediaFileUploadRequest
+import truvideo.sdk.common.exception.TruvideoSdkException
+
+
+/**
+ * Interface for receiving transfer-related events during file upload.
+ */
+interface TruvideoSdkMediaFileUploadCallback {
+
+    /**
+     * Called when a file transfer operation is completed successfully.
+     *
+     * @param id The unique key associated with the completed transfer operation.
+     * @param response The response with URL or location where the file has been successfully transferred.
+     * It also has the Tags, transcriptionUrl and transcriptionLength.
+     */
+    fun onComplete(id: String, response: TruvideoSdkMediaFileUploadRequest)
+
+    /**
+     * Called when the progress of a file transfer operation changes.
+     *
+     * @param id The unique key associated with the ongoing transfer operation.
+     * @param progress The current progress of the transfer operation as a percentage.
+     */
+    fun onProgressChanged(id: String, progress: Float)
+
+    /**
+     * Called when an error occurs during a file transfer operation.
+     *
+     * @param id The unique key associated with the transfer operation that encountered an error.
+     * @param ex The exception that describes the error encountered during the transfer.
+     */
+    fun onError(id: String, ex: TruvideoSdkException)
+}
