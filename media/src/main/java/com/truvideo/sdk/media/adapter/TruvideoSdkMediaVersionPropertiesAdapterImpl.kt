0f04e92a6cf0a9e1f76dc992170776584235544b
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-08-05 21:18:57 +0000

    Update 73.1.2-rc.56

---
+++ media/src/main/java/com/truvideo/sdk/media/adapter/TruvideoSdkMediaVersionPropertiesAdapterImpl.kt
@@ -0,0 +1,42 @@
+package com.truvideo.sdk.media.adapter
+
+import android.content.Context
+import com.truvideo.sdk.media.interfaces.TruvideoSdkMediaVersionPropertiesAdapter
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+
+internal class TruvideoSdkMediaVersionPropertiesAdapterImpl(
+    val context: Context
+) : TruvideoSdkMediaVersionPropertiesAdapter {
+
+    override fun readProperty(propertyName: String): String? {
+        val assetManager = context.assets
+        val filename = "version.properties"
+
+        return try {
+            val inputStream = assetManager.open(filename)
+            val reader = BufferedReader(InputStreamReader(inputStream))
+
+            var line: String?
+            while (reader.readLine().also { line = it } != null) {
+                val parts = line?.split("=")
+                if (parts?.size == 2) {
+                    val key = parts[0].trim()
+                    val value = parts[1].trim()
+
+                    if (key == propertyName) {
+                        reader.close()
+                        return value
+                    }
+                }
+            }
+
+            reader.close()
+            null
+        } catch (e: IOException) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
