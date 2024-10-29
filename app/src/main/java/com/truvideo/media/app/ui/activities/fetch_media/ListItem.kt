1689b2768ca98bc969f8bb9f36de0210b86a6d3e
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-10-29 01:06:14 +0000

    Update 76.2.1-beta.72

---
+++ app/src/main/java/com/truvideo/media/app/ui/activities/fetch_media/ListItem.kt
@@ -0,0 +1,108 @@
+package com.truvideo.media.app.ui.activities.fetch_media
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.truvideo.media.app.ui.theme.TruvideosdkmediaTheme
+import com.truvideo.sdk.media.model.TruvideoSdkMediaMetadata
+import com.truvideo.sdk.media.model.TruvideoSdkMediaResponse
+import com.truvideo.sdk.media.model.TruvideoSdkMediaResponseType
+import com.truvideo.sdk.media.model.TruvideoSdkMediaTags
+import java.util.Date
+
+@Composable
+fun MediaResponseListItem(
+    model: TruvideoSdkMediaResponse
+) {
+    Card(modifier = Modifier.fillMaxWidth()) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text("Id", style = MaterialTheme.typography.titleSmall)
+            Text(model.id, style = MaterialTheme.typography.bodySmall)
+
+            Box(modifier = Modifier.height(8.dp))
+
+            Text("Title", style = MaterialTheme.typography.titleSmall)
+            Text(model.title, style = MaterialTheme.typography.bodySmall)
+
+            Box(modifier = Modifier.height(8.dp))
+
+            Text("Type", style = MaterialTheme.typography.titleSmall)
+            Text(model.type.name, style = MaterialTheme.typography.bodySmall)
+
+            Box(modifier = Modifier.height(8.dp))
+
+            Text("Created Date", style = MaterialTheme.typography.titleSmall)
+            Text(model.createdDate.toString(), style = MaterialTheme.typography.bodySmall)
+
+            Box(modifier = Modifier.height(8.dp))
+
+            Text("Url", style = MaterialTheme.typography.titleSmall)
+            Text(model.url, style = MaterialTheme.typography.bodySmall)
+
+            Box(modifier = Modifier.height(8.dp))
+
+            Text("Transcription Url", style = MaterialTheme.typography.titleSmall)
+            Text(model.transcriptionUrl, style = MaterialTheme.typography.bodySmall)
+
+            Box(modifier = Modifier.height(8.dp))
+
+            Text("Transcription Length", style = MaterialTheme.typography.titleSmall)
+            Text(model.transcriptionLength?.toString() ?: "", style = MaterialTheme.typography.bodySmall)
+
+            Box(modifier = Modifier.height(8.dp))
+
+            Text("Tags", style = MaterialTheme.typography.titleSmall)
+            Text(model.tags.toJson(), style = MaterialTheme.typography.bodySmall)
+
+            Box(modifier = Modifier.height(8.dp))
+
+            Text("Metadata", style = MaterialTheme.typography.titleSmall)
+            Text(model.metadata.toJson(), style = MaterialTheme.typography.bodySmall)
+
+            Box(modifier = Modifier.height(8.dp))
+
+            Text("Include In Report", style = MaterialTheme.typography.titleSmall)
+            Text(model.includeInReport.toString(), style = MaterialTheme.typography.bodySmall)
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun Test() {
+    TruvideosdkmediaTheme {
+        MediaResponseListItem(
+            model = TruvideoSdkMediaResponse(
+                title = "title",
+                id = "id",
+                transcriptionLength = 0f,
+                transcriptionUrl = "transcription url",
+                tags = TruvideoSdkMediaTags.builder()
+                    .set("key", "value")
+                    .build(),
+                type = TruvideoSdkMediaResponseType.Video,
+                url = "url",
+                createdDate = Date(),
+                includeInReport = false,
+                metadata = TruvideoSdkMediaMetadata.builder()
+                    .set("key", "value")
+                    .set("list", listOf("value1", "value2"))
+                    .set(
+                        "nested", TruvideoSdkMediaMetadata.builder()
+                            .set("key", "value")
+                            .set("list", listOf("value1", "value2")).build()
+                    )
+                    .build()
+            )
+        )
+    }
+}
---
+++ media/src/main/java/com/truvideo/sdk/media/util/FileUriUtil.kt
@@ -0,0 +1,128 @@
+package com.truvideo.sdk.media.util
+
+import android.content.ContentUris
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import android.webkit.MimeTypeMap
+import truvideo.sdk.common.exceptions.TruvideoSdkException
+
+internal object FileUriUtil {
+
+    fun getMimeType(context: Context, uri: Uri): String {
+        val contentResolver = context.contentResolver
+        var mimeType = contentResolver.getType(uri)
+
+        return if (mimeType != null) {
+            mimeType
+        } else {
+            val fileExtension = MimeTypeMap.getFileExtensionFromUrl(uri.toString())
+            mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(fileExtension)
+            mimeType ?: "unknown/unknown"
+        }
+    }
+
+    fun isPhotoOrVideo(context: Context, uri: Uri): Boolean {
+        // Get the MIME type of the file from the URI
+        val mimeType = getMimeType(context, uri)
+
+        // Check if the file extension is one of the common image or video extensions
+        val isImage = mimeType.startsWith("image/")
+        val isVideo = mimeType.startsWith("video/")
+
+        // Return true if it's an image or video, false otherwise
+        return isImage || isVideo
+    }
+
+    fun getExtension(context: Context, uri: Uri): String {
+        val contentResolver = context.contentResolver
+        val projection = arrayOf(MediaStore.MediaColumns.DISPLAY_NAME)
+        val cursor = contentResolver.query(uri, projection, null, null, null)
+        var extension: String? = null
+        if (cursor != null && cursor.moveToFirst()) {
+            var columnIndex = cursor.getColumnIndex(MediaStore.MediaColumns.DISPLAY_NAME)
+            if (columnIndex < 0) {
+                columnIndex = 0
+            }
+            val displayName = cursor.getString(columnIndex)
+            cursor.close()
+
+            displayName?.let {
+                val lastDot = it.lastIndexOf(".")
+                if (lastDot != -1) {
+                    extension = it.substring(lastDot + 1)
+                }
+            }
+        }
+        if (extension != null) {
+            return extension as String
+        } else {
+            val uriSplit = uri.toString().split(".")
+            if (uriSplit.isNotEmpty()) {
+                return uriSplit.last()
+            }
+        }
+
+        throw TruvideoSdkException("Invalid file")
+    }
+
+    fun realPathFromUri(context: Context, uri: Uri): String? {
+        if (DocumentsContract.isDocumentUri(context, uri)) {
+            if (isExternalStorageDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                val type = split[0]
+                if ("primary".equals(type, ignoreCase = true)) return Environment.getExternalStorageDirectory().toString() + "/" + split[1]
+            } else if (isDownloadsDocument(uri)) {
+                val id = DocumentsContract.getDocumentId(uri)
+                val contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), java.lang.Long.valueOf(id))
+                return getDataColumn(context, contentUri, null, null)
+            } else if (isMediaDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                val type = split[0]
+                var contentUri: Uri? = null
+                when (type) {
+                    "image" -> contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    "video" -> contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                    "audio" -> contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+                }
+                val selection = "_id=?"
+                val selectionArgs = arrayOf(split[1])
+                return getDataColumn(context, contentUri, selection, selectionArgs)
+            }
+        } else if ("content".equals(uri.scheme, ignoreCase = true)) {
+            return if (isGooglePhotosUri(uri)) uri.lastPathSegment else getDataColumn(context, uri, null, null)
+        } else if ("file".equals(uri.scheme, ignoreCase = true)) {
+            return uri.path
+        }
+        return null
+    }
+
+    private fun isGooglePhotosUri(uri: Uri): Boolean = "com.google.android.apps.photos.content" == uri.authority
+
+    private fun getDataColumn(context: Context, uri: Uri?, selection: String?, selectionArgs: Array<String>?): String? {
+        val column = "_data"
+        val projection = arrayOf(column)
+        val cursor = if (uri != null) context.contentResolver.query(uri, projection, selection, selectionArgs, null) else null
+        cursor.use {
+            if (it != null && it.moveToFirst()) {
+                val index = it.getColumnIndexOrThrow(column)
+                val result = it.getString(index)
+                it.close()
+                return result
+            }
+        }
+        return null
+    }
+
+    private fun isExternalStorageDocument(uri: Uri): Boolean = "com.android.externalstorage.documents" == uri.authority
+
+    private fun isDownloadsDocument(uri: Uri): Boolean = "com.android.providers.downloads.documents" == uri.authority
+
+    private fun isMediaDocument(uri: Uri): Boolean = "com.android.providers.media.documents" == uri.authority
+
+
+}
