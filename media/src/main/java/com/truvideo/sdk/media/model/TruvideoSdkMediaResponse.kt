7a5f1e68d039f625457b18fa14f89c375f22f66b
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-11-15 19:08:42 +0000

    Update 76.2.5-dev.84

---
+++ media/src/main/java/com/truvideo/sdk/media/model/TruvideoSdkMediaResponse.kt
@@ -0,0 +1,52 @@
+package com.truvideo.sdk.media.model
+
+import MetadataSerializer
+import TagsSerializer
+import com.truvideo.sdk.media.model.serializers.IsoDateSerializer
+import com.truvideo.sdk.media.model.serializers.TruvideoSdkMediaResponseTypeSerializer
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.util.Date
+
+@Serializable
+data class TruvideoSdkMediaResponse(
+    val id: String,
+
+    @Serializable(with = TruvideoSdkMediaResponseTypeSerializer::class)
+    val type: TruvideoSdkMediaResponseType = TruvideoSdkMediaResponseType.Unknown,
+
+    val title: String = "",
+    val url: String,
+    val transcriptionUrl: String = "",
+    val transcriptionLength: Float?,
+    val includeInReport: Boolean?,
+
+    @Serializable(with = IsoDateSerializer::class)
+    val createdDate: Date,
+
+    @Serializable(with = TagsSerializer::class)
+    val tags: TruvideoSdkMediaTags = TruvideoSdkMediaTags(),
+
+    @Serializable(with = MetadataSerializer::class)
+    val metadata: TruvideoSdkMediaMetadata = TruvideoSdkMediaMetadata()
+) {
+    fun toJson(): String = jsonConfig.encodeToString(this)
+
+    companion object {
+        @OptIn(ExperimentalSerializationApi::class)
+        private val jsonConfig = Json {
+            ignoreUnknownKeys = true
+            isLenient = true
+            allowStructuredMapKeys = true
+            prettyPrint = true
+            useArrayPolymorphism = true
+            explicitNulls = false
+        }
+
+        fun fromJson(json: String): TruvideoSdkMediaResponse {
+            return jsonConfig.decodeFromString(json)
+        }
+    }
+}
